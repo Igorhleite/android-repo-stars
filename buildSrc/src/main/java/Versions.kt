68c8d7ad70a524be492d800aff1f7956a475b9c1
igorhenriqueleite <ygor.mxl@gmail.com>
2023-11-16 23:03:55 -0300

    Add: adding base dependencies and adjusting gradle files

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -5,6 +5,24 @@ object Versions {
     const val material_version = "1.5.0"
     const val constraintLayout_version = "2.1.4"
 
+    //Navigation
+    const val nav_version = "2.4.0-alpha05"
+
+    // ViewModel and LiveData
+    const val lifecycle_version = "2.5.1"
+
+    //Hilt
+    const val hilt_version = "2.39.1"
+
+    //Retrofit
+    const val retrofit_version = "2.9.0"
+
+    //Coroutines
+    const val coroutines_version = "1.5.1"
+
+    //Shimmer
+    const val shimmer_version = "0.5.0"
+
     // Test libs versions
     const val junit_version = "4.13.2"
     const val androidxJunit_version = "1.1.5"
