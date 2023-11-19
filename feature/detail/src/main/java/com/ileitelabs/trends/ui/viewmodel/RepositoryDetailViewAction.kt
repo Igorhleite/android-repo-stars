efd8d3671d03dc884795e15d9239d84397cacafe
igorhenriqueleite <ygor.mxl@gmail.com>
2023-11-19 12:31:32 -0300

    Add: adding repository visit action on detail

--- feature/detail/src/main/java/com/ileitelabs/trends/ui/viewmodel/RepositoryDetailViewAction.kt
+++ feature/detail/src/main/java/com/ileitelabs/trends/ui/viewmodel/RepositoryDetailViewAction.kt
@@ -2,4 +2,5 @@ package com.ileitelabs.trends.ui.viewmodel
 
 internal sealed class RepositoryDetailViewAction {
     object FetchData : RepositoryDetailViewAction()
+    data class GoToExternalUrl(val url: String) : RepositoryDetailViewAction()
 }
--- foundation/core/src/main/java/com/ileitelabs/core/data/remote/di/AuthInterceptor.kt
+++ foundation/core/src/main/java/com/ileitelabs/core/data/remote/di/AuthInterceptor.kt
@@ -1,18 +1,21 @@
 package com.ileitelabs.core.data.remote.di
 
+import com.ileitelabs.repotrends.foundation.core.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class AuthInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request().newBuilder()
-            .addHeader(
-                "Authorization",
-                "Bearer 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"
-            )
-            .addHeader("X-GitHub-Api-Version", "2022-11-28")
-            .build()
 
-        return chain.proceed(request)
+        if (BuildConfig.ACCESS_TOKEN.isNotBlank()) {
+            request
+                .addHeader(
+                    "Authorization",
+                    BuildConfig.ACCESS_TOKEN
+                )
+                .addHeader("X-GitHub-Api-Version", "2022-11-28")
+        }
+        return chain.proceed(request.build())
     }
 }
